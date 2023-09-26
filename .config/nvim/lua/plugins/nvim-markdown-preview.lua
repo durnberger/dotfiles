481e25080a2044f445b4eddb3c3673301ab03987
durnberger <paul@durnberger.co.uk>
2023-09-26 11:28:41 +0100

    automated: 2023-09-26-1128

--- .config/nvim/lua/plugins/nvim-markdown-preview.lua
+++ .config/nvim/lua/plugins/nvim-markdown-preview.lua
@@ -1,10 +1,7 @@
 return {
    "davidgranstrom/nvim-markdown-preview",
 
-    lazy = true,
-
     config = function()
-        
         -- preferences
         vim.g['nvim_markdown_preview_theme'] = github
         vim.g['nvim_markdown_preview_format'] = markdown
@@ -12,6 +9,5 @@ return {
         -- keymap
         local keymap = vim.keymap
         keymap.set({"n", "i"}, "<leader>m", ":MarkdownPreview<cr>", {})
-
     end,
 }
